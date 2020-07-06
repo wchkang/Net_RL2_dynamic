c54178ce8219255fc2f309232d374551cd765517
Woochul Kang <woochul@inu.ac.kr>
2020-07-07 00:15:06 +0900

    bug fix

--- train_cifar100_finetune.py
+++ train_cifar100_finetune.py
@@ -291,7 +291,10 @@ for param in net.fc.parameters():
     param.requires_grad = True
 
 print('\n######### Finetuning High-Performance Model ###########\n')
-  
+
+best_acc = 0
+best_acc_top5 = 0
+
 checkpoint = torch.load('./checkpoint/' + 'CIFAR100-' + args.model + "-S" + str(args.shared_rank) + "-U" + str(args.unique_rank) + "-L" + str(args.lambdaR) + "-" + args.visible_device + '.pth')
 net.load_state_dict(checkpoint['net_state_dict'])
 best_acc = checkpoint['acc']
