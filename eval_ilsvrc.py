3ea060f1a76ab47ff7ad412459b5503c6e7c1479
Woochul Kang <wchkang@inu.ac.kr>
2020-08-17 21:37:29 +0900

    DataParallel is default network type

--- eval_ilsvrc.py
+++ eval_ilsvrc.py
@@ -56,7 +56,8 @@ class MyDataParallel(nn.DataParallel):
 if torch.cuda.device_count() >= 1:
     print("Let's use", torch.cuda.device_count(), "GPUs!")
     # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-    net = MyDataParallel(net)
+
+net = MyDataParallel(net)
 
 
 #Eval for models
