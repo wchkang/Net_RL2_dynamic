e772a6fa0ee1290f198a72c596206724f50cbdd9
Woochul Kang <wchkang@inu.ac.kr>
2021-01-23 14:16:40 +0900

    support for ResNet/skip

--- modelsize.py
+++ modelsize.py
@@ -16,21 +16,25 @@ import numpy as np
 
 #from models.cifar100 import mobilenetv2_skip
 #from models.cifar10 import mobilenetv2_cifar10_skip
+#from models.cifar100 import resnet_skip
 #from models.ilsvrc import mobilenetv2_skip
-from models.cifar100 import resnet_skip
+from models.ilsvrc import mobilenetv2_skip_pytorch
+from models.ilsvrc import resnet_skip_imagenet
 
 from ptflops import get_model_complexity_info
 
 #args.visible_device sets which cuda devices to be used"
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 device='cuda'
 
 #model = mobilenetv2_skip.MobileNetV2_skip
 #model = mobilenetv2_skip.MobileNetV2
 #model = mobilenetv2_cifar10_skip.MobileNetV2_skip
 #model = mobilenetv2_skip.MobileNetV2_skip
-model = resnet_skip.ResNet50_skip
+model = mobilenetv2_skip_pytorch.MobileNetV2_skip
+#model = resnet_skip_imagenet.ResNet34_skip
+#model = torchvision.models.resnet34(pretrained=False) 
 
 with torch.cuda.device(0):
   net = model(num_classes=1000)
@@ -47,7 +51,7 @@ with torch.cuda.device(0):
   x = torch.randn(1,3,224,224, device='cuda')
   y = net(x)
   t_start = time.time()
-  for i in range(100):
+  for i in range(1000):
       y = net(x)
   t_end = time.time()
   print('time: {:.3f} seconds per inference'.format((t_end - t_start)/100.0))
