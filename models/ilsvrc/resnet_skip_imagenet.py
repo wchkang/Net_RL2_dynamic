52b6f67f4a7922a14065beba869969f11a656d4e
Woochul Kang <wchkang@inu.ac.kr>
2021-01-23 13:32:05 +0900

    add support for skippable BasicBlock

--- models/ilsvrc/resnet_skip_imagenet.py
+++ models/ilsvrc/resnet_skip_imagenet.py
@@ -4,36 +4,47 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 
-# class BasicBlock(nn.Module):
-#     expansion = 1
+class BasicBlock(nn.Module):
+    expansion = 1
 
-#     def __init__(self, in_planes, planes, stride=1):
-#         super(BasicBlock, self).__init__()
+    def __init__(self, in_planes, planes, stride=1, skippable=False):
+        super(BasicBlock, self).__init__()
         
-#         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-#         self.bn1 = nn.BatchNorm2d(planes)
-#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-#         self.bn2 = nn.BatchNorm2d(planes)
+        self.skippable = skippable 
 
-#         self.shortcut = nn.Sequential()
-#         if stride != 1 or in_planes != planes:
-#             self.shortcut = nn.Sequential(
-#                 nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-#                 nn.BatchNorm2d(self.expansion * planes)
-#             )
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-#     def forward(self, x):
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = F.relu(out, inplace=True)
+        if (self.skippable==True):
+            self.conv2_skip = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn2_skip = nn.BatchNorm2d(planes)
 
-#         out = self.conv2(out)
-#         out = self.bn2(out)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
 
-#         out += self.shortcut(x)
-#         out = F.relu(out, inplace=True)
+    def forward(self, x, skip=False):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out, inplace=True)
+
+
+        if (self.skippable == True and skip==True):
+            out = self.conv2_skip(out)
+            out = self.bn2_skip(out)
+        else:            
+            out = self.conv2(out)
+            out = self.bn2(out)
+
+        out += self.shortcut(x)
+        out = F.relu(out, inplace=True)
         
-#         return out
+        return out
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -94,11 +105,68 @@ class SkippableSequential(nn.Sequential):
 
         return out
 
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        layers = []
+        layers.append(block(self.in_planes, planes, stride, skippable=False))
+        self.in_planes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+
+        return SkippableSequential(*layers)
+
+    def forward(self, x, skip=False):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x, inplace=True)
+
+        x = self.layer1(x, skip)
+        x = self.layer2(x, skip)
+        x = self.layer3(x, skip)
+        x = self.layer4(x, skip)
+        
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+     
+        return x
+
 
 class ResNet_skip(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=100):
+    def __init__(self, block, num_blocks, num_classes=1000):
         super(ResNet_skip, self).__init__()
         self.in_planes = 64
+
+        if issubclass(block, BasicBlock):
+            #print("Block")
+            self.block_type = 'BasicBlock'
+        elif issubclass(block, Bottleneck):
+            #print("Bottleneck")
+            self.block_type = 'Bottleneck'
+        else:
+            print("unknow block type")
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
@@ -171,8 +239,14 @@ class ResNet_skip(nn.Module):
         # defreeze low-perf-exclusive parameters and BNs
         for i in range(1,5):
             layer = getattr(self, "layer"+str(i))
-            layer[0].conv3_skip.weight.requires_grad = True
-            layer[0].bn3_skip.train()
+            if self.block_type == 'Bottleneck':
+                layer[0].conv3_skip.weight.requires_grad = True
+                layer[0].bn3_skip.train()
+            elif self.block_type == 'BasicBlock':
+                layer[0].conv2_skip.weight.requires_grad = True
+                layer[0].bn2_skip.train()
+            else:
+                print("[Error] Unknown block type")
 
     def freeze_lowperf(self):
         """ Freeze low-performance-exclusive-exclusive and shared layers """
@@ -182,10 +256,17 @@ class ResNet_skip(nn.Module):
         # defreeze params of only being used by the high-performance model
         for i in range(1,5):
             layer = getattr(self, "layer"+str(i))
-            layer[0].conv3.weight.requires_grad = True
-            layer[0].bn3.train()
+            if self.block_type == 'Bottleneck':
+                layer[0].conv3.weight.requires_grad = True
+                layer[0].bn3.train()
+            elif self.block_type == 'BasicBlock':
+                layer[0].conv2.weight.requires_grad = True
+                layer[0].bn2.train()
+            else:
+                print("[Error] Unknown block type")
 
-            num_skip = round(len(layer)/2.0)
+
+            num_skip = len(layer)//2
             for j in range(1, num_skip+1):
                 for param in layer[j].parameters():
                     param.requires_grad = True
@@ -195,8 +276,12 @@ class ResNet_skip(nn.Module):
 #def ResNet18(num_classes=100):
 #     return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
 
-# def ResNet34(num_classes=100):
-#     return ResNet(BasicBlock, [3, 4, 6, 3], num_classes)
+
+def ResNet34(num_classes=1000):
+    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes)
+
+def ResNet34_skip(num_classes=1000):
+    return ResNet_skip(BasicBlock, [3, 4, 6, 3], num_classes)
 
 def ResNet50_skip(num_classes=1000):
     return ResNet_skip(Bottleneck, [3, 4, 6, 3], num_classes)
@@ -205,11 +290,12 @@ def ResNet101_skip(num_classes=1000):
     return ResNet_skip(Bottleneck, [3, 4, 23, 3], num_classes)
 
 def test():
-    net = ResNet101_skip()
+    #net = ResNet34_skip()
+    net = ResNet34()
     #x = torch.randn(256,3,32,32)
     x = torch.randn(16,3,224,224)
-    y = net(x, False)
-    print(y.size())
+    y = net(x, True)
     print(net)
+    print(y.size())
 
 #test()
